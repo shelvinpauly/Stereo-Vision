82becefc7b1d1c42cb234fda34c1a813d422059a
Shelvin Pauly <spauly@umd.edu>
2023-08-13 20:13:00 -0500

    .

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # Stereo-Vision
+
+## Project Description
+
+In this project, I have implemented the concept of Stereo Vision on three data sets, each of them containing 2 images of the same scenario but taken from two different camera angles. By comparing the information about the scene from 2 vantage points, we can obtain the 3D information by examining the relative position of the objects. I have also estimated the camera pose using the concept of the Essential matrix (E), Fundamental matrix (F) and, triangulation. 
+
+## Pipeline
+### 1. Calibration 
+- Compare the two images and select the set of matching feature. Tune the Lowe's ration to reject the outliers
+- Estimate the Fundamental matrix using the obtained matching feature. Use the RANSAC to make your estimation more robust. Enforce the rank 2 condition for the fundamental matrix.
+- Estimate the Essential matrix(E) from the Fundamental matrix(F) and instrinsic camera parameter.
+- Decompose the E into a translation T and rotation R
+- Disambugiate the T and R using triangulation.
+
+### 2. Rectification
+- Apply perspective transfomation to make sure that the epipolar lines are horizontal for both the images. This will limit the search space to horizontal line during the corrospondace matching process in the later stage.
+
+### 3. Corrospondence
+- For each epipolar line, apply the sliding window with SSD to find the corrospondence and calulate disparity.
+- Rescale the disparity to be from 0-255 for visualization
+
+### 4. Compute Depth Image
+- Using the disparity calculated above, compute the depth map. The resultant image has a depth image instead of disparity.
+
+## Dataset
+
+[MiddleBury Stereo Dataset](https://vision.middlebury.edu/stereo/data/scenes2021/#description)
+
+## Result
+
+- Matching features in left and right image
+![Feature_matching](https://user-images.githubusercontent.com/90370308/215355870-3752aa49-60f7-4364-9f0d-456060480ce0.png)
+
+- Epipolar line corrosponding to the obtained matching feature
+![Matching_Points_and_Epipolar_line](https://user-images.githubusercontent.com/90370308/215355914-a54e76fe-34c0-45aa-bed6-9196f453f382.png)
+
+- Rectified epipolar lines
+![Rectified_Epipolar](https://user-images.githubusercontent.com/90370308/215359693-9471be64-2e45-4125-9cd5-1ae7914ae24a.png)
+
+- Disparity and Depth map
+![final_map](https://user-images.githubusercontent.com/90370308/215364599-e8ef55ee-bfe5-4b03-b2bf-8e7d0549e738.png)
+
+- Disparity and Depth heat map
+![final_heatmap](https://user-images.githubusercontent.com/90370308/215364607-21834a6d-c15c-4f24-969f-235d2b5caef3.png)
+
+## Requirement
+1. Python 2.0 or above
+2. OpenCV
+
+## License
+
+ [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+Copyright (c) Jan 2023 Pradip Kathiriya
+
---
+++ pendulum.py
@@ -0,0 +1,488 @@
+import numpy as np
+import cv2 as cv
+import copy
+import matplotlib.pyplot as plt
+import DrawCameras
+import math
+
+# Checks if a matrix is a valid rotation matrix.
+def isRotationMatrix(R):
+    """To check if the matrix id rotation matrix
+    Args:
+        R (TYPE): Input matrix
+    Returns:
+        TYPE: Norm of image
+    """
+    Rt = np.transpose(R)
+
+    shouldBeIdentity = np.dot(Rt, R)
+    I = np.identity(3, dtype=R.dtype)
+    n = np.linalg.norm(I - shouldBeIdentity)
+    return n < 1e-6
+
+
+# Calculates rotation matrix to euler angles
+def rotationMatrixToEulerAngles(R):
+    """Convert Rotation Matrix to Euler angless
+    Args:
+        R (TYPE): Rotation Matrix
+    Returns:
+        TYPE: Angles x,y,z in radians
+    """
+    assert (isRotationMatrix(R))
+
+    sy = math.sqrt(R[0, 0] * R[0, 0] + R[1, 0] * R[1, 0])
+
+    singular = sy < 1e-6
+
+    if not singular:
+        x = math.atan2(R[2, 1], R[2, 2])
+        y = math.atan2(-R[2, 0], sy)
+        z = math.atan2(R[1, 0], R[0, 0])
+    else:
+        x = math.atan2(-R[1, 2], R[1, 1])
+        y = math.atan2(-R[2, 0], sy)
+        z = 0
+
+    return np.array([x, y, z])
+
+# compute fundamental matrix
+def getFunamentalMatrix(L,R):
+    
+    A = np.array([[L[0][0]*R[0][0] , L[0][0]*R[0][1], L[0][0] , L[0][1]*R[0][0] , L[0][1]*R[0][1] , L[0][1] , R[0][0] , R[0][1] , 1],
+                  [L[1][0]*R[1][0] , L[1][0]*R[1][1], L[1][0] , L[1][1]*R[1][0] , L[1][1]*R[1][1] , L[1][1] , R[1][0] , R[1][1] , 1],
+                  [L[2][0]*R[2][0] , L[2][0]*R[2][1], L[2][0] , L[2][1]*R[2][0] , L[2][1]*R[2][1] , L[2][1] , R[2][0] , R[2][1] , 1],
+                  [L[3][0]*R[3][0] , L[3][0]*R[3][1], L[3][0] , L[3][1]*R[3][0] , L[3][1]*R[3][1] , L[3][1] , R[3][0] , R[3][1] , 1],
+                  [L[4][0]*R[4][0] , L[4][0]*R[4][1], L[4][0] , L[4][1]*R[4][0] , L[4][1]*R[4][1] , L[4][1] , R[4][0] , R[4][1] , 1],
+                  [L[5][0]*R[5][0] , L[5][0]*R[5][1], L[5][0] , L[5][1]*R[5][0] , L[5][1]*R[5][1] , L[5][1] , R[5][0] , R[5][1] , 1],
+                  [L[6][0]*R[6][0] , L[6][0]*R[6][1], L[6][0] , L[6][1]*R[6][0] , L[6][1]*R[6][1] , L[6][1] , R[6][0] , R[6][1] , 1],
+                  [L[7][0]*R[7][0] , L[7][0]*R[7][1], L[7][0] , L[7][1]*R[7][0] , L[7][1]*R[7][1] , L[7][1] , R[7][0] , R[7][1] , 1],
+                  ])
+    
+    _, _, V_t = np.linalg.svd(A)
+    V = V_t.T
+    F = np.reshape(V[:, -1],(3,3))
+    F = np.round_(F, decimals=3)
+    
+    if np.linalg.matrix_rank(F) == 3: 
+
+        U,S,V = np.linalg.svd(F)
+        w = np.zeros((3,3))
+        w[0,0] = S[1]
+        w[1,1] = S[2]
+        F = np.dot(U,np.dot(w,V))
+        F = np.round_(F, decimals=3)
+    return F
+
+def bestFundamentalMatrix(list_kp1, list_kp2):
+    epsilon = 0.3
+    best_F = []
+    highest_inlier = 0
+    num_iteration = 5000
+    best_kp1 = []
+    best_kp2 = []
+    for i in range(num_iteration):
+        idx = np.random.choice(len(list_kp1) , 8 , replace=False)
+        kp1 = list_kp1[idx,:]
+        kp2 = list_kp2[idx,:]
+        F = getFunamentalMatrix(kp1,kp2)
+
+        inlier = 0
+        for i in range(len(list_kp1)):
+            x = np.array([list_kp1[i][0], list_kp1[i][1], 1])
+            x_t = np.array([list_kp2[i][0], list_kp2[i][1], 1]).T
+            if abs(np.dot(x_t,np.dot(F,x))) < epsilon:
+                inlier += 1
+
+        if inlier > highest_inlier:
+
+            highest_inlier = inlier
+            best_F = F
+            best_kp1 = kp1
+            best_kp2 = kp2      
+    return best_F, best_kp1, best_kp2
+
+def extractCameraPose(essential_matrix):
+    
+    R_set = []
+    C_set = []
+    
+    U,_,V = np.linalg.svd(essential_matrix)
+    
+    W = np.array([[0,-1,0],[1,0,0],[0,0,1]])
+    
+    C1 = U[:,-1]
+    R1 = np.dot(U,np.dot(W,V))
+    if np.linalg.det(R1) == -1:
+        R1 = -R1
+        C1 = -C1
+    R_set.append(R1)
+    C_set.append(C1)
+        
+    C2 = U[:,-1]
+    R2 = np.dot(U,np.dot(W.T,V))
+    if np.linalg.det(R2) == -1:
+        R2 = -R2
+        C2 = -C2
+    R_set.append(R2)
+    C_set.append(C2)
+        
+    C3 = -U[:,-1]
+    R3 = np.dot(U,np.dot(W,V))
+    if np.linalg.det(R3) == -1:
+        R3 = -R3
+        C3 = -C3
+    R_set.append(R3)
+    C_set.append(C3)
+        
+    C4 = -U[:,-1]
+    R4 = np.dot(U,np.dot(W.T,V))
+    if np.linalg.det(R4) == -1:
+        R4 = -R4
+        C4 = -C4
+    R_set.append(R4)
+    C_set.append(C4)
+        
+    return R_set, C_set
+
+# Visualize epilines
+# def drawlines(img1src, lines, pts1src):
+
+#     r, c,_= img1src.shape
+#     np.random.seed(0)
+#     for r, pt1 in zip(lines, pts1src):
+#         color = tuple(np.random.randint(0, 255, 3).tolist())
+#         x0, y0 = map(int, [0, -r[2]/r[1]])
+#         x1, y1 = map(int, [c, -(r[2]+r[0]*c)/r[1]])
+#         img1color = cv.line(img1src, (x0, y0), (x1, y1), color, 1)
+#         img1color = cv.circle(img1color, tuple(pt1), 5, color, -1)
+#     return img1color
+
+def getMatchingFeature(image1, image2):
+    
+    des = cv.ORB_create()
+    kp1, des1 = des.detectAndCompute(image1, None)
+    kp2, des2 = des.detectAndCompute(image2, None)
+
+    bf = cv.BFMatcher()
+    matches = bf.knnMatch(des1, des2, k=2)
+
+    lowe_ratio = 0.45
+
+    best_matches = []
+    for m,n in matches:
+        if m.distance < lowe_ratio*n.distance:
+            best_matches.append(m)
+            
+    draw_pt = []
+    for m,n in matches:
+        if m.distance < lowe_ratio*n.distance:
+            draw_pt.append([m])
+            
+    list_kp1 = np.int32([kp1[mat.queryIdx].pt for mat in best_matches])
+    list_kp2 = np.int32([kp2[mat.trainIdx].pt for mat in best_matches])
+    final_image = cv.drawMatchesKnn(image1,kp1,image2,kp2,draw_pt,None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    
+    return final_image , list_kp1, list_kp2
+
+def calculateDisparity(image1, image2):
+
+    disparity_map = np.zeros((image2.shape[0], image2.shape[1]), dtype=np.float64)
+    
+    for i in range(5,image2.shape[1]-5):
+        for j in range(5, image2.shape[0]-5):
+            
+            E_1 = image2[j-5:j+5,i-5:i+5]
+            
+            SSD_max = np.inf
+            corrosponding_index = 0
+            for k in range(i-100, i+100, 3):
+                if (k-5) < 0 or (k+5) > image1.shape[1]-1:
+                    continue
+                
+                E_2 = image1[j-5:j+5,k-5:k+5]
+                
+                difference = E_1 - E_2
+                difference = np.array(difference)
+                element_square = np.square(difference)
+                SSD = element_square.sum()
+                if SSD < SSD_max:
+                    SSD_max = SSD
+                    corrosponding_index = k      
+            disparity_map[j][i] = i - corrosponding_index
+            
+    disparity_map = disparity_map + np.abs(np.min(disparity_map)) + 1
+    disparity_map = (disparity_map/np.max(disparity_map))*255
+    disparity_map = disparity_map.astype(np.uint8)
+    return disparity_map     
+
+# def calculateDisparity(image1, image2):
+    
+#     disparity_map = np.zeros((image2.shape[0], image2.shape[1]), dtype=np.float64)
+    
+    
+
+def computeDepthMap(image, baseline, focal_x):
+    
+    image = image.astype(np.float64)
+    depth_map = (baseline * focal_x)/image
+    depth_map[depth_map > 5000] = 5000
+    depth_map = (depth_map/np.max(depth_map))*255
+    depth_map = depth_map.astype(np.uint8)
+    
+    return depth_map 
+
+def projection_matrix(K, R, C):
+    
+    A = np.concatenate((np.identity(3),-C), axis=1)
+    P = np.dot(K,np.dot(R,A))
+    
+    return P
+
+#Function to find 3D point given 2D pixel locations
+def linear_triangulation(R_set, C_set, left_pts, right_pts, K1, K2):
+    
+    x3D_set = []
+
+    for i in range(len(R_set)):
+        R1, R2 = np.identity(3), R_set[i]
+        C1, C2 = np.zeros((3, 1)),  C_set[i].reshape(3,1)
+       
+        P1 = projection_matrix(K1, R1, C1)
+        P2 = projection_matrix(K2, R2, C2)
+       
+        p1, p2, p3 = P1
+        p1_, p2_, p3_ = P2
+       
+        p1, p2, p3 =  p1.reshape(1,-1), p2.reshape(1,-1), p3.reshape(1,-1)
+        p1_, p2_, p3_ =  p1_.reshape(1,-1), p2_.reshape(1,-1), p3_.reshape(1,-1)
+       
+        x3D =[]
+       
+        for left_pt, right_pt in zip(left_pts, right_pts):
+            x, y = left_pt
+            x_, y_ = right_pt
+            A = np.vstack((y * p3 - p2, p1 - x * p3, y_ * p3_ - p2_, p1_-x_ * p3_ ))
+            _, _, Vt = np.linalg.svd(A)
+            X = Vt[-1]
+            X = X / X[-1]
+            x3D.append(X[:3])
+         
+        x3D_set.append(x3D)
+
+    return x3D_set
+
+def disambiguateCameraPose(R_set, C_set, x3D_set):
+    best_i = 0
+    max_positive_depths = 0
+   
+    for i in range(len(R_set)):
+        n_positive_depths=  0
+        R, C = R_set[i],  C_set[i].reshape(-1, 1)
+        r3 = R[2].reshape(1, -1)
+        x3D = x3D_set[i]
+       
+        for X in x3D:
+            X = X.reshape(-1, 1)
+            if r3 @ (X - C) > 0 and X[2] > 0:
+                n_positive_depths += 1
+               
+        if n_positive_depths > max_positive_depths:
+            best_i = i
+            max_positive_depths = n_positive_depths
+
+    R, C = R_set[best_i], C_set[best_i]
+
+    return R, C
+
+def drawlines(img1, img2, lines, pts1, pts2):
+    
+    r, c,_ = img1.shape
+    # print(r)
+    # print(c)
+      
+    for r, pt1, pt2 in zip(lines, pts1, pts2):
+          
+        color = tuple(np.random.randint(0, 255,
+                                        3).tolist())
+          
+        x0, y0 = map(int, [0, -r[2] / r[1] ])
+        x1, y1 = map(int, 
+                     [c, -(r[2] + r[0] * c) / r[1] ])
+          
+        img1 = cv.line(img1, 
+                        (x0, y0), (x1, y1), color, 1)
+        img1 = cv.circle(img1,
+                          tuple(pt1), 5, color, -1)
+        img2 = cv.circle(img2, 
+                          tuple(pt2), 5, color, -1)
+    return img1, img2
+
+def main():
+
+    pendulum_left = cv.imread('data/pendulum/im0.png',1)
+    pendulum_left = cv.resize(pendulum_left, (int(pendulum_left.shape[1]*0.5), int(pendulum_left.shape[0]*0.5)))
+    pendulum_right = cv.imread('data/pendulum/im1.png',1)
+    pendulum_right = cv.resize(pendulum_right, (int(pendulum_right.shape[1]*0.5), int(pendulum_right.shape[0]*0.5)))
+    # cv.imshow('pendulum_left', pendulum_left)
+    # cv.imshow('pendulum_right', pendulum_right)
+
+    final_image, list_kp1, list_kp2 = getMatchingFeature(pendulum_left, pendulum_right)
+    # cv.imshow('Final_image', final_image)
+    # cv.imwrite('Feature_matching.png', final_image)
+
+    kp1 = copy.copy(list_kp1)
+    kp2 = copy.copy(list_kp2)
+    F, best_kp1, best_kp2 = bestFundamentalMatrix(list_kp1, list_kp2)
+    # kp1 = list_kp1[:8]
+    # kp2 = list_kp2[:8]
+    # F = getFunamentalMatrix(kp1,kp2)
+    # best_kp1 = list_kp1[:8]
+    # best_kp2 = list_kp2[:8]
+    # print('=======')
+    # print('Fundamental Matrix')
+    # print(F)
+    
+    K = np.array([[1729.05, 0, -364.24],[0, 1729.05, 552.22], [0, 0, 1]])
+    
+    # # essentail matrix
+    E = np.dot(np.dot(K.T,F),K)
+    U,S,V = np.linalg.svd(E)
+    S = np.diag(S)
+    S[-1,-1] = 0
+    S[1,1] = 1
+    S[0,0] = 1
+    E = np.dot(np.dot(U,S),V)
+    # print('=======')
+    # print('Essential Matrix')
+    # print(E)
+
+    R_set, C_set = extractCameraPose(E)
+    
+    K1 = np.array([[1729.05, 0, -364.24],[0, 1729.05, 552.22], [0, 0, 1]])
+    K2 = np.array([[1729.05, 0, -364.24],[0, 1729.05, 552.22], [0, 0, 1]])
+
+    x3D_set = linear_triangulation(R_set, C_set, kp1, kp2, K1, K2)
+ 
+    ###############################
+    ###### Plot camera pose #######
+    ###############################
+    # ax = plt.axes(projection='3d')
+    # # Data for three-dimensional scattered points
+    # ax.scatter3D(
+    #     x3D_set[0], x3D_set[1], x3D_set[2],
+    #     s=1)  # cmap='viridis',
+    # ax.set_xlabel('x')
+    # ax.set_ylabel('y')
+    # ax.set_zlabel('z')
+    # ax.set_xlim([-0.5, 1])
+    # ax.set_ylim([-0.5, 1])
+    # ax.set_zlim([0, 1.5])
+    # R, C = disambiguateCameraPose(R_set, C_set, x3D_set)
+    # R = rotationMatrixToEulerAngles(R)
+    # R = np.rad2deg(R)
+    # R1 = np.identity(3)
+    # C1= np.zeros((3, 1))
+    # R1 = rotationMatrixToEulerAngles(R1)
+    # R1 = np.rad2deg(R1)
+    # plt.scatter(x3D_set[0], x3D_set[2], s=1)
+    # plt.plot(
+    #         C[0],
+    #         C[2],
+    #         marker=(3, 0, int(R[1])),
+    #         markersize=15,
+    #         linestyle='None')
+    # plt.plot(
+    #         C1[0],
+    #         C1[2],
+    #         marker=(3, 0, int(R1[1])),
+    #         markersize=15,
+    #         linestyle='None')
+    # # for i in range(0, len(C_set)):
+    # #     R1 = rotationMatrixToEulerAngles(R_set[i])
+    # #     R1 = np.rad2deg(R1)
+    # #     plt.plot(
+    # #         C_set[i][0],
+    # #         C_set[i][2],
+    # #         marker=(3, 0, int(R1[1])),
+    # #         markersize=15,
+    # #         linestyle='None')
+    # ax1 = plt.gca()
+    # ax1.set_xlabel('x')
+    # ax1.set_ylabel('z')
+    # # ax.set_xlim([-0.5, 1])
+    # # ax.set_ylim([-0.5, 2])
+    # plt.show()
+    
+    R, C = disambiguateCameraPose(R_set, C_set, x3D_set)
+    # print('=======')
+    # print('camera rotation')
+    # print(R)
+    # print('=======')
+    # print('camera translation')
+    # print(C)
+    
+      
+    # Find epipolarlines corresponding to points in right image (second image) and
+    # drawing its lines on left image
+    lines1 = cv.computeCorrespondEpilines(list_kp2.reshape(-1, 1, 2), 2, F)
+    lines1 = lines1.reshape(-1, 3)
+    pendulum_left_copy = copy.copy(pendulum_left)
+    # Find epilines corresponding to points in left image (first image) and
+    # drawing its lines on right image
+    lines2 = cv.computeCorrespondEpilines(list_kp1.reshape(-1, 1, 2), 2, F)
+    lines2 = lines2.reshape(-1, 3)
+    pendulum_right_copy = copy.copy(pendulum_left)
+    
+    pendulum_left_epiline,_ = drawlines(pendulum_left_copy,pendulum_right_copy, lines1, list_kp1, list_kp2)
+    pendulum_right_epiline,_ = drawlines(pendulum_right_copy, pendulum_left_copy, lines2, list_kp2, list_kp1)
+    image = np.concatenate((pendulum_left_epiline,pendulum_right_epiline), axis =1)
+    # cv.imshow('Matching_Points_and_Epipolar_line.png', image)
+    # cv.imwrite('Matching_Points_and_Epipolar_line.png', image)
+
+
+    h1, w1,_ = pendulum_left.shape
+    h2, w2,_ = pendulum_right.shape
+    _, H1, H2 = cv.stereoRectifyUncalibrated( np.float32(best_kp1), np.float32(best_kp2), F, imgSize=(w1, h1))
+    print(H1)
+    print(best_kp1)
+    pendulum_left_rectified_epi = cv.warpPerspective(pendulum_left_epiline, H1, (w1, h1))
+    pendulum_right_rectified_epi = cv.warpPerspective(pendulum_right_epiline, H2, (w2, h2))
+    image = np.concatenate((pendulum_left_rectified_epi,pendulum_right_rectified_epi), axis =1)
+    # cv.imshow('Rectified Epipolar Line', image)
+    # cv.imwrite('Rectified_Epipolar.png', image)
+
+    pendulum_left_rectified = cv.warpPerspective(pendulum_left, H1, (w1, h1))
+    pendulum_right_rectified = cv.warpPerspective(pendulum_right, H2, (w2, h2))
+    
+    pendulum_left_rect_gray = cv.cvtColor(pendulum_left_rectified , cv.COLOR_BGR2GRAY)
+    pendulum_right_rect_gray = cv.cvtColor(pendulum_right_rectified , cv.COLOR_BGR2GRAY)
+    
+    disparity_map = calculateDisparity(pendulum_left_rect_gray, pendulum_right_rect_gray)
+    disparity_heatmap = cv.applyColorMap(disparity_map, cv.COLORMAP_JET)
+    # cv.imshow('Disparity Map', disparity_map)
+    cv.imwrite('Disparity_Map.png', disparity_map)
+    # cv.imshow('Disparity Heatmap', disparity_heatmap)
+    cv.imwrite('Disparity_Heatmap.png', disparity_heatmap)
+    
+    baseline = 537.75
+    f_x = 1729.05
+    depth_map = computeDepthMap(disparity_map,baseline, f_x )
+    depth_heatmap = cv.applyColorMap(depth_map, cv.COLORMAP_JET)
+    # cv.imshow('Depth Map', depth_map)
+    cv.imwrite('Depth_Map.png', depth_map)
+    # cv.imshow('Depth Heatmap', depth_heatmap)
+    cv.imwrite('Depth_Heatmap.png', depth_heatmap)
+
+    img1 = np.hstack([disparity_map, depth_map])
+    img2 = np.hstack([disparity_heatmap, depth_heatmap])
+    cv.imwrite('final_map.png', img1)
+    cv.imwrite('final_heatmap.png', img2)
+    cv.imshow('map',img1 )
+    cv.waitKey(0)
+    
+    
+       
+    
+if __name__ == '__main__':
+    main()
